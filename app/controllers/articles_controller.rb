deea85bf9aae2ddcd5a93147c3bcb5e5f14efc4c
yeasayer <mardybm@gmail.com>
2013-08-12 19:16:43 +0700

    removed unused controllers and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,4 @@
 class ArticlesController < ApplicationController
-    def score
-        @article = Article.where(:score.gt => params[:score]).limit(10)
-    end
-
     def show
         @article = Article.where(artist: params[:id])
     end
