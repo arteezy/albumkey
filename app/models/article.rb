1d134153bd88433ab836f93c9b6f137a4a7f6049
Arthur L <mardybm@gmail.com>
2014-09-24 02:42:40 +0700

    added search method to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,12 @@ class Article
   include Mongoid::Document
   #field :id, type: String
   #field :_id, type: String, default: ->{ id }
+
+  def self.search(search)
+    if search
+      where(artist: Regexp.new("#{search}", true)).distinct(:artist)
+    else
+      all.distinct(:artist)
+    end
+  end
 end
