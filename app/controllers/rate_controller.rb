dd7feb19e300a7b8da70a112af3a911a39846dbd
Arthur L <mardybm@gmail.com>
2015-01-03 21:38:32 +0600

    generated controller for Rates

---
+++ app/controllers/rate_controller.rb
@@ -0,0 +1,4 @@
+class RateController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'rate/create'
+
   root to: 'articles#index'
 
   devise_for :users
