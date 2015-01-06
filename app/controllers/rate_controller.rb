e3fb6719c3f1d55d51d583b078760d98a3af6c3f
Arthur L <mardybm@gmail.com>
2015-01-06 21:52:48 +0600

    added create rate method

--- app/controllers/rate_controller.rb
+++ app/controllers/rate_controller.rb
@@ -1,4 +1,6 @@
 class RateController < ApplicationController
   def create
+    Rate.create(user_id: current_user.id, article_id: params[:id], rate: params[:rate])
+    redirect_to article_path(Article.find(params[:id])["artist"])
   end
 end
