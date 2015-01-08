f3adacccef349f26eea0fa06d04cbc565d635549
Arthur L <mardybm@gmail.com>
2015-01-09 00:37:02 +0600

    create and update Rate optimizatons

--- app/controllers/rate_controller.rb
+++ app/controllers/rate_controller.rb
@@ -1,6 +1,12 @@
 class RateController < ApplicationController
   def create
-    Rate.create(user_id: current_user.id, article_id: params[:id], rate: params[:rate])
+    rate = Rate.where(user_id: current_user.id, article_id: params[:id])
+    if rate.exists?
+      rate.update(rate: params[:rate].to_f)
+    else
+      rate.create(rate: params[:rate].to_f)
+    end
+
     redirect_to article_path(Article.find(params[:id])["artist"])
   end
 end
