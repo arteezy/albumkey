c5a3bf9711aab284a4a466655e77827063e32f15
Arthur L <mardybm@gmail.com>
2015-01-31 18:54:31 +0600

    add search album by artist

--- app/models/album.rb
+++ app/models/album.rb
@@ -11,4 +11,12 @@ class Album
   field :score, type: Float
   field :bnm, type: Mongoid::Boolean
   field :bnr, type: Mongoid::Boolean
+
+  def self.search_by_artist(artist)
+    if artist
+      where(artist: Regexp.new("#{artist}", true)).distinct(:artist).sort
+    else
+      all.distinct(:artist).sort
+    end
+  end
 end
