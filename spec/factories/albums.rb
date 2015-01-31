9c6b87e8a6d4b1093ccf92e7922d2410ae98ad9c
Arthur L <mardybm@gmail.com>
2015-01-31 15:49:42 +0600

    add real data to album factory

--- spec/factories/albums.rb
+++ spec/factories/albums.rb
@@ -1,14 +1,14 @@
 FactoryGirl.define do
 
   factory :album do
-    title "MyString"
-    artist "MyString"
-    label "MyString"
-    year "MyString"
-    date "MyString"
-    artwork "MyString"
-    url "MyString"
-    score 1.5
+    title "Everyday Robots"
+    artist "Damon Albarn"
+    label "Parlophone"
+    year "2014"
+    date "2014-04-28"
+    artwork "http://cdn2.pitchfork.com/albums/20489/homepage_large.099546ba.jpg"
+    url "http://pitchfork.com/reviews/albums/19280-damon-albarn-everyday-robots/"
+    score 7.0
     bnm false
     bnr false
   end
