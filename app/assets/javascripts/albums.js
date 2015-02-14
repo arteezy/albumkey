0a95de3cc7b378118b4df1a2dafb6cc11d1f5896
Arthur L <mardybm@gmail.com>
2015-02-15 02:20:47 +0600

    fix JS to work with Turbolinks

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).on("ready page:load", function() {
   $("#filter").submit(function() {
     $(this).find(":input").filter(function(){ return !this.value; }).attr("disabled", "disabled");
     return true;
