50ced2aa3aceda7afe517abfedc5f9ebb2b5d486
Arthur L <mardybm@gmail.com>
2015-03-16 04:03:45 +0600

    fix JS for Turbolinks

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -3,30 +3,30 @@ $(document).on("ready page:load", function() {
     $(this).find(":input").filter(function(){ return !this.value; }).attr("disabled", "disabled");
     return true;
   });
-});
 
-$(function () {
-  $("div.score").mouseenter(function() {
-    $(this).removeClass("dim");
+  $(function () {
+    $("div.score").mouseenter(function() {
+      $(this).removeClass("dim");
 
-    $(this).mousemove(function(e) {
-      $(this).text(((this.offsetTop + this.offsetHeight - e.pageY) / this.offsetHeight * 10.0).toFixed(1));
-      // $(this).text(((e.pageX - this.offsetLeft) / this.offsetWidth * 10.0 + 0.1).toFixed(1));
+      $(this).mousemove(function(e) {
+        $(this).text(((this.offsetTop + this.offsetHeight - e.pageY) / this.offsetHeight * 10.0).toFixed(1));
+        // $(this).text(((e.pageX - this.offsetLeft) / this.offsetWidth * 10.0 + 0.1).toFixed(1));
+      });
+
+      $(this).mouseleave(function() {
+        $(this).text("0.0");
+        $(this).addClass("dim");
+      });
     });
 
-    $(this).mouseleave(function() {
-      $(this).text("0.0");
-      $(this).addClass("dim");
+    $("div.score").mouseleave(function() {
+      $(this).off("mousemove");
     });
-  });
 
-  $("div.score").mouseleave(function() {
-    $(this).off("mousemove");
-  });
-
-  $("div.score").mouseup(function() {
-    $(this).off("mousemove");
-    $(this).off("mouseleave");
-    $(this).removeClass("dim");
+    $("div.score").mouseup(function() {
+      $(this).off("mousemove");
+      $(this).off("mouseleave");
+      $(this).removeClass("dim");
+    });
   });
 });
