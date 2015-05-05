94b6e1a9f71c4dfc11a0fbd335849b59776daacb
Arthur L <mardybm@gmail.com>
2015-05-05 17:51:57 +0600

    fix album factory rating generator

--- spec/factories/albums.rb
+++ spec/factories/albums.rb
@@ -5,9 +5,9 @@ FactoryGirl.define do
     label { Faker::Company.name }
     year { Faker::Time.between(10.years.ago, Time.now).year }
     date { Faker::Time.between(10.years.ago, Time.now) }
-    artwork { Faker::Avatar.image("album-artwork", "300x300", "jpg") }
+    artwork { Faker::Avatar.image('artwork', '300x300', 'jpg') }
     url { Faker::Internet.url }
-    score { rand(0.0...10.0) }
+    score { rand(0.0...10.0).round(1) }
     bnm false
     bnr false
   end
