8c5def8773dc2d6fdadf29874844e3b49ae9443c
Arthur L <mardybm@gmail.com>
2015-07-01 04:27:31 +0600

    add new test for Rate model

--- spec/models/rate_spec.rb
+++ spec/models/rate_spec.rb
@@ -4,4 +4,10 @@ describe Rate, type: :model do
   it 'has a valid factory' do
     expect(build(:rate)).to be_valid
   end
+
+  it 'is invalid without a rate' do
+    rate = build(:rate, rate: nil)
+    rate.valid?
+    expect(rate.errors[:rate]).to include("can't be blank")
+  end
 end
