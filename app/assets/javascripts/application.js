59a898bdaf3eaa0a41e23de87858bbacba822d0c
Arthur L <mardybm@gmail.com>
2015-07-07 14:58:35 +0600

    remove old range slider plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
-//= require bootstrap-slider
 //= require bootstrap3-typeahead
 //= require ion.rangeSlider
 //= require_tree .
