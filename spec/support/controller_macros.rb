317a93a820652a88cdcd458c3e56af4c6b163c8a
Arthur L <mardybm@gmail.com>
2015-07-12 07:20:56 +0600

    simplify authentification

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,15 +2,14 @@ module ControllerMacros
   def login_admin
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:admin]
-      sign_in FactoryGirl.create(:admin)
+      sign_in create(:admin)
     end
   end
 
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryGirl.create(:user)
-      sign_in user
+      sign_in create(:user)
     end
   end
 end
