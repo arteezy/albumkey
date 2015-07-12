be6ea77b7104dbc7885d76e303968e7dcd4927e4
Arthur L <mardybm@gmail.com>
2015-07-12 07:20:02 +0600

    add factory for invalid rate

--- spec/factories/rates.rb
+++ spec/factories/rates.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
     rate { rand(0.0...10.0).round(1) }
     association :user, factory: :user
     association :album, factory: :album
+
+    factory :invalid_rate do
+      rate -13.3
+    end
   end
 end
