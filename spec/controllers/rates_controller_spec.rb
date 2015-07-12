539db9bf6f93775ea14847d5117aa05aa1b9fd26
Arthur L <mardybm@gmail.com>
2015-07-12 07:21:45 +0600

    add spec for RatesController

---
+++ spec/controllers/rates_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe RatesController, type: :controller do
+  describe "POST #create" do
+    login_user
+
+    before :each do
+      @album = create(:album)
+    end
+
+    context "with valid params" do
+      it "creates a new Rate" do
+        expect {
+          post :create, attributes_for(:rate, id: @album)
+        }.to change(Rate, :count).by(1)
+      end
+
+      it "responds with HTTP 201 Created" do
+        post :create, attributes_for(:rate, id: @album)
+        expect(response).to have_http_status(:created)
+      end
+
+      it "doesn't redirect to the created Rate" do
+        post :create, attributes_for(:rate, id: @album)
+        expect(response).to_not redirect_to(Rate.last)
+      end
+    end
+
+    context "with invalid params" do
+      it "doesn't create a new Rate" do
+        expect {
+          post :create, attributes_for(:invalid_rate, id: @album)
+        }.to_not change(Rate, :count)
+      end
+
+      it "responds with HTTP 400 Bad Request" do
+        post :create, attributes_for(:invalid_rate, id: @album)
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+  end
+end
