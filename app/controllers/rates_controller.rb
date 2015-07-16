c811bf0097c5d56b1686d67fcda3401af787e451
Arthur L <mardybm@gmail.com>
2015-07-16 11:05:23 +0600

    rewrite create action without upsert shenanigans

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -2,9 +2,9 @@ class RatesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    rate = Rate.new(user: current_user, album_id: params[:album_id], rate: params[:rate])
-    if rate.valid?
-      rate.upsert
+    rate = Rate.find_or_initialize_by(user: current_user, album_id: params[:album_id])
+    rate.rate = params[:rate]
+    if rate.save
       render nothing: true, status: 201
     else
       render nothing: true, status: 400
