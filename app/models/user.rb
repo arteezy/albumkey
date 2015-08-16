5661fee4892237f1bae603deff2d001476234396
Arthur L <mardybm@gmail.com>
2015-08-16 06:48:01 +0600

    destroy rates if their author is destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,5 +37,5 @@ class User
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
 
-  has_many :rates
+  has_many :rates, dependent: :destroy
 end
