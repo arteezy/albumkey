69d83eaa997a180cc33ce4381a5e286810930bcd
Arthur L <mardybm@gmail.com>
2015-08-27 16:17:50 +0600

    use shorter syntax for routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   resources :albums
   root to: 'albums#index'
   devise_for :users
-  get '/search', to: 'albums#search'
-  get '/stats', to: 'albums#stats'
+  get 'search', to: 'albums#search'
+  get 'stats', to: 'albums#stats'
 
   scope :api do
-    get '/artists', to: 'albums#artists'
-    get '/labels', to: 'albums#labels'
-    post '/rate/:album_id', to: 'rates#create', as: 'rate'
+    get 'artists', to: 'albums#artists'
+    get 'labels', to: 'albums#labels'
+    post 'rate/:album_id', to: 'rates#create', as: 'rate'
   end
 end
