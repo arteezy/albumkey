1f95aac6564584d1a5698602e2d6217efc31b5d4
Arthur L <mardybm@gmail.com>
2015-09-02 19:47:57 +0600

    supress DB logging messages to not clutter test repots

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,6 +38,9 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # Supress DB logging messages to not clutter test repots
+  Mongo::Logger.logger.level = Logger::WARN
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
