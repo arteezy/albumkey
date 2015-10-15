479d2cab58d47c0be733bed04513a9caa06d18ef
Arthur L <mardybm@gmail.com>
2015-10-15 14:25:20 +0600

    refactor Albums filter view with helpers

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,2 +1,10 @@
 module AlbumsHelper
+  def toggle_button(name, value)
+    active = true if params[name] == value
+
+    haml_tag :div, class: "btn btn-primary #{name} #{'active' if active}" do
+      haml_tag :input, type: 'radio', name: name, id: "#{name}_#{value}", value: value, checked: active
+      haml_tag :span, class: "glyphicon glyphicon-sort-by-attributes#{'-alt' if value == 'asc'}"
+    end
+  end
 end
