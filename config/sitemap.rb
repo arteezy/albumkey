e9740490b77b23bd4fb320adb290cfc1d4f1f00b
Arthur L <mardybm@gmail.com>
2015-10-21 10:25:32 +0600

    configure sitemap generation

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,27 +1,12 @@
-# Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = 'http://richfork.ml'
 
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
+  add stats_path, priority: 0.5, changefreq: 'weekly'
+  add search_path, priority: 0.5, changefreq: 'weekly'
+  add new_user_session_path, priority: 0.3, changefreq: 'monthly'
+  add new_user_registration_path, priority: 0.3, changefreq: 'monthly'
+
+  Album.each do |album|
+    add album_path(album), priority: 0.8, changefreq: 'daily'
+  end
 end
