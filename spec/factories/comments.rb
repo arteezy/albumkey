94a8529ffa31a192396c530afe38495c8becf2af
Arthur L <mardybm@gmail.com>
2015-11-15 23:59:38 +0600

    add factory with random fake data for comments

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :comment do
-    user_email "MyString"
-body "MyText"
-album_id "MyString"
-user_id "MyString"
+    user_email { Faker::Internet.email }
+    user_avatar { Faker::Avatar.image('avatar', '33x33', 'jpg') }
+    body { Faker::Lorem.sentence }
+    association :user, factory: :user
+    association :album, factory: :album
   end
-
 end
