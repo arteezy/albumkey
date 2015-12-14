84363154de5285d27343f2d39ee1b98971ea1316
Arthur L <mardybm@gmail.com>
2015-12-14 21:14:52 +0600

    rewrite comments controller spec

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,143 +1,143 @@
 require 'rails_helper'
 
-RSpec.describe CommentsController, type: :controller do
+describe CommentsController, type: :controller do
   before :each do
     @album = create(:album)
+    @user = create(:user)
+    sign_in @user
   end
 
-  # This should return the minimal set of attributes required to create a valid
-  # Comment. As you add validations to Comment, be sure to
-  # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    skip('Add a hash of attributes valid for your model')
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    skip('Add a hash of attributes invalid for your model')
   }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # CommentsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
-  describe "GET #index" do
-    it "assigns all comments as @comments" do
-      comment = Comment.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:comments)).to eq([comment])
+  describe 'GET #index' do
+    it 'assigns all comments as @comments' do
+      comment = create(:comment)
+      get :index, { album_id: comment.album }, valid_session
+      expect(assigns(:comments)).to match_array([comment])
     end
   end
 
-  describe "GET #show" do
-    it "assigns the requested comment as @comment" do
-      comment = Comment.create! valid_attributes
-      get :show, { id: comment.to_param }, valid_session
+  describe 'GET #show' do
+    it 'assigns the requested comment as @comment' do
+      comment = create(:comment)
+      get :show, { id: comment, album_id: comment.album }, valid_session
       expect(assigns(:comment)).to eq(comment)
     end
   end
 
-  describe "GET #new" do
-    it "assigns a new comment as @comment" do
+  describe 'GET #new' do
+    it 'assigns a new comment as @comment' do
       get :new, { album_id: @album }, valid_session
       expect(assigns(:comment)).to be_a_new(Comment)
     end
   end
 
-  describe "GET #edit" do
-    it "assigns the requested comment as @comment" do
-      comment = Comment.create! valid_attributes
-      get :edit, {:id => comment.to_param}, valid_session
+  describe 'GET #edit' do
+    it 'assigns the requested comment as @comment' do
+      comment = create(:comment)
+      get :edit, { id: comment, album_id: comment.album }, valid_session
       expect(assigns(:comment)).to eq(comment)
     end
   end
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Comment" do
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new Comment' do
         expect {
-          post :create, {:comment => valid_attributes}, valid_session
-        }.to change(Comment, :count).by(1)
+          post :create, { comment: attributes_for(:comment) }.merge(album_id: @album, user_id: @user), valid_session
+        }.to change { @album.reload.comments.count }.by(1)
       end
 
-      it "assigns a newly created comment as @comment" do
-        post :create, {:comment => valid_attributes}, valid_session
+      it 'assigns a newly created comment as @comment' do
+        post :create, { comment: attributes_for(:comment) }.merge(album_id: @album, user_id: @user), valid_session
         expect(assigns(:comment)).to be_a(Comment)
         expect(assigns(:comment)).to be_persisted
       end
 
-      it "redirects to the created comment" do
-        post :create, {:comment => valid_attributes}, valid_session
-        expect(response).to redirect_to(Comment.last)
+      it 'redirects to album page with the created comment' do
+        post :create, { comment: attributes_for(:comment) }.merge(album_id: @album, user_id: @user), valid_session
+        expect(response).to redirect_to(@album)
       end
     end
 
-    context "with invalid params" do
-      it "assigns a newly created but unsaved comment as @comment" do
-        post :create, {:comment => invalid_attributes}, valid_session
+    context 'with invalid params' do
+      it 'assigns a newly created but unsaved comment as @comment' do
+        post :create, { comment: attributes_for(:invalid_comment) }.merge(album_id: @album, user_id: @user), valid_session
         expect(assigns(:comment)).to be_a_new(Comment)
       end
 
-      it "re-renders the 'new' template" do
-        post :create, {:comment => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
+      it 'redirects to album page' do
+        post :create, { comment: attributes_for(:invalid_comment) }.merge(album_id: @album, user_id: @user), valid_session
+        expect(response).to redirect_to(@album)
       end
     end
   end
 
-  describe "PUT #update" do
-    context "with valid params" do
+  describe 'PUT #update' do
+    context 'with valid params' do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        { body: 'Good vibes' }
       }
 
-      it "updates the requested comment" do
-        comment = Comment.create! valid_attributes
-        put :update, {:id => comment.to_param, :comment => new_attributes}, valid_session
+      it 'updates the requested comment' do
+        comment = create(:comment)
+        put :update, { album_id: comment.album, id: comment, comment: new_attributes }, valid_session
         comment.reload
-        skip("Add assertions for updated state")
+        expect(comment.body).to eq('Good vibes')
       end
 
-      it "assigns the requested comment as @comment" do
-        comment = Comment.create! valid_attributes
-        put :update, {:id => comment.to_param, :comment => valid_attributes}, valid_session
+      it 'assigns the requested comment as @comment' do
+        comment = create(:comment)
+        put :update, { album_id: comment.album, id: comment, comment: new_attributes }, valid_session
         expect(assigns(:comment)).to eq(comment)
       end
 
-      it "redirects to the comment" do
-        comment = Comment.create! valid_attributes
-        put :update, {:id => comment.to_param, :comment => valid_attributes}, valid_session
-        expect(response).to redirect_to(comment)
+      it 'redirects to album page' do
+        comment = create(:comment)
+        put :update, { album_id: comment.album, id: comment, comment: new_attributes }, valid_session
+        expect(response).to redirect_to(comment.album)
       end
     end
 
-    context "with invalid params" do
-      it "assigns the comment as @comment" do
-        comment = Comment.create! valid_attributes
-        put :update, {:id => comment.to_param, :comment => invalid_attributes}, valid_session
+    context 'with invalid params' do
+      let(:invalid_attributes) {
+        { body: '' }
+      }
+
+      it 'assigns the comment as @comment' do
+        comment = create(:comment)
+        put :update, { album_id: comment.album, id: comment, comment: invalid_attributes }, valid_session
         expect(assigns(:comment)).to eq(comment)
       end
 
-      it "re-renders the 'edit' template" do
-        comment = Comment.create! valid_attributes
-        put :update, {:id => comment.to_param, :comment => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
+      it 're-renders the edit template' do
+        comment = create(:comment)
+        put :update, { album_id: comment.album, id: comment, comment: invalid_attributes }, valid_session
+        expect(response).to render_template('edit')
       end
     end
   end
 
-  describe "DELETE #destroy" do
-    it "destroys the requested comment" do
-      comment = Comment.create! valid_attributes
+  describe 'DELETE #destroy' do
+    it 'destroys the requested comment' do
+      comment = create(:comment)
       expect {
-        delete :destroy, {:id => comment.to_param}, valid_session
-      }.to change(Comment, :count).by(-1)
+        delete :destroy, { album_id: comment.album, id: comment }, valid_session
+      }.to change { comment.album.reload.comments.count }.by(-1)
     end
 
-    it "redirects to the comments list" do
-      comment = Comment.create! valid_attributes
-      delete :destroy, {:id => comment.to_param}, valid_session
-      expect(response).to redirect_to(comments_url)
+    it 'redirects to album page' do
+      comment = create(:comment)
+      delete :destroy, { album_id: comment.album, id: comment }, valid_session
+      expect(response).to redirect_to(comment.album)
     end
   end
 end
