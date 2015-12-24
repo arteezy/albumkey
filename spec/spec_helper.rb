d47034cd89ad2b5efc304ea0f80ecdcdf864331d
Arthur L <mardybm@gmail.com>
2015-12-24 14:45:04 +0600

    use CodeClimate test reporter only on CI machines and SimpleCov on local machines

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
-require 'codeclimate-test-reporter'
-
-CodeClimate::TestReporter.start
+if ENV['CI']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start
+end
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
