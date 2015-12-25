92de8a9b63dc3091229c074e1a7c95fee013384f
Arthur L <mardybm@gmail.com>
2015-12-25 21:47:27 +0600

    add Admin user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,10 @@ FactoryGirl.define do
     username { Faker::Internet.user_name }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
+    role :user
+
+    factory :admin do
+      role :admin
+    end
   end
 end
