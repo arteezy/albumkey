03287d71ca9b47c8a3f823f28ac5160d4f92f3f3
Arthur L <mardybm@gmail.com>
2016-02-05 23:12:32 +0600

    remove old unused helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,4 @@ module ApplicationHelper
   def title(page_title)
     content_for :title, "#{page_title} | Richfork"
   end
-
-  def meta_tag(tag, text)
-    content_tag :"meta_#{tag}", text
-  end
-
-  def yield_meta_tag(tag, default_text = '')
-    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
-  end
 end
