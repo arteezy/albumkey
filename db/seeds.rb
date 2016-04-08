639ff9ea109b76e64b9278739824292cb2d46eeb
Arthur L <mardybm@gmail.com>
2016-04-08 16:36:24 +0600

    remove db/ folder, never need it with mongodb

--- db/seeds.rb
+++
@@ -1,3 +0,0 @@
-puts 'Creating Admin user...'
-user = User.create! :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'User: ' + user.name + ' has been created'
