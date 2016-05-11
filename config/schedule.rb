63facbac7f5225c3ca6962773f239a6beecb0781
Arthur L <mardybm@gmail.com>
2016-05-11 13:25:10 +0600

    update whenever config to work properly with rbenv

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,13 @@
 
 # Learn more: http://github.com/javan/whenever
 
+set :rbenv, '"$HOME/.rbenv/shims":"$HOME/.rbenv/bin"'
+
+job_type :rake,   'cd :path && PATH=:rbenv:"$PATH" :environment_variable=:environment bundle exec rake :task --silent :output'
+job_type :runner, 'cd :path && PATH=:rbenv:"$PATH" bin/rails runner -e :environment ":task" :output'
+
+set :output, 'log/whenever.log'
+
 every 6.hours do
   rake 'albums:update'
 end
