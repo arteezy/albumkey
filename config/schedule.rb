f15841a87c56fff7179df20205d3248891427974
Arthur L <mardybm@gmail.com>
2016-05-14 18:55:14 +0600

    ajdust cron timer to ensure correct parsing

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,3 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
-
 set :rbenv, '"$HOME/.rbenv/shims":"$HOME/.rbenv/bin"'
 
 job_type :rake,   'cd :path && PATH=:rbenv:"$PATH" :environment_variable=:environment bundle exec rake :task --silent :output'
@@ -30,7 +9,7 @@ every 6.hours do
   rake 'albums:update'
 end
 
-every :day, at: '05:01am' do
+every :day, at: '05:05am' do
   rake 'albums:getlatest'
 end
 
