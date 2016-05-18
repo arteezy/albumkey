3e0a6489317d5d32c15244bb3589ba087bf39fc3
Arthur L <mardybm@gmail.com>
2016-05-18 19:08:29 +0600

    add Discogs data & Youtube videos to show page

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -20,6 +20,19 @@ function ready() {
     return true;
   });
 
+  $('.discogs > .panel-heading').click(function() {
+    var $this = $(this);
+    if ($this.hasClass('panel-collapsed')) {
+      $this.closest('.panel').find('.panel-body').slideDown();
+      $this.removeClass('panel-collapsed');
+      $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+    } else {
+      $this.closest('.panel').find('.panel-body').slideUp();
+      $this.addClass('panel-collapsed');
+      $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+    }
+  });
+
   // Triple-state button for BNM selection
   $('#bnm-btn').click(function() {
     input = $('#bnm');
