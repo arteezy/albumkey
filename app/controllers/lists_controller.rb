650d92aca5763ca0f07226f1aa1b6dccc5ed352b
Arthur L <mardybm@gmail.com>
2016-12-29 20:49:08 +0600

    join Lists query with Users

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,7 +4,7 @@ class ListsController < ApplicationController
   # GET /lists
   # GET /lists.json
   def index
-    @lists = List.all
+    @lists = List.includes(:user).all
   end
 
   # GET /lists/1
