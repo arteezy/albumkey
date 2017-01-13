d8656e7bf1a2951225016941ceb15208e3f1e9a3
Arthur L <mardybm@gmail.com>
2017-01-13 17:04:47 +0600

    add tests for Lists, back to 100% coverage

--- spec/factories/lists.rb
+++ spec/factories/lists.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
   factory :list do
-    title 'MyString'
+    title { Faker::Lorem.sentence }
     category :personal
     association :user, factory: :user
+
+    factory :invalid_list do
+      title nil
+    end
   end
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:list)).to be_valid
+  end
+
+  it 'is invalid without a title' do
+    list = build(:list, title: nil)
+    list.valid?
+    expect(list.errors[:title]).to include("can't be blank")
+  end
+
+  it 'is invalid without a category' do
+    list = build(:list, category: nil)
+    list.valid?
+    expect(list.errors[:category]).to include("can't be blank")
+  end
+
+  it 'is invalid without a user' do
+    list = build(:list, user: nil)
+    list.valid?
+    expect(list.errors[:user]).to include("can't be blank")
+  end
 end
