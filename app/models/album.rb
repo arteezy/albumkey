6a1b87aeaaf037782a3adc7364b94c287d649bf1
Arthur L <mardybm@gmail.com>
2017-04-10 14:02:54 +0600

    album position in list is now set via association hook

--- app/models/album.rb
+++ app/models/album.rb
@@ -41,7 +41,7 @@ class Album
   # but allows multiple documents without the key.
   index({ p4k_id: 1 }, unique: true, sparse: true)
 
-  has_and_belongs_to_many :lists
+  has_and_belongs_to_many :lists, after_add: :set_position
   has_many :rates, dependent: :destroy
   embeds_many :comments # implies dependent: :destroy
 
@@ -67,5 +67,11 @@ class Album
     else
       all
     end
+  end
+
+  private
+
+  def set_position(list)
+    list.positions << list.positions.size + 1
   end
 end
