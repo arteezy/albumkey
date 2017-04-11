6703dd3e188cc042fc753c8f97eb669e5862b0f9
Arthur L <mardybm@gmail.com>
2017-04-11 14:05:52 +0600

    cover all current lists functionality with feature tests

--- spec/features/user_works_with_lists_spec.rb
+++ spec/features/user_works_with_lists_spec.rb
@@ -43,4 +43,70 @@ feature 'User works with lists' do
     visit lists_path
     expect(page).not_to have_content 'Bad List'
   end
+
+  scenario 'adds album to list' do
+    fill_in 'list_title', with: 'Awesome List'
+    choose 'list_category_personal'
+    click_button 'Create List'
+
+    FactoryGirl.create(:album, title: 'Cool Album')
+    visit albums_path
+    first('.card h3 > a').click
+    select 'Awesome List', from: 'album_list_id'
+    click_button 'Add to List'
+
+    expect(page).to have_content 'Album was successfully added to this List'
+
+    visit lists_path
+    click_link 'Awesome List'
+    expect(page).to have_content 'Cool Album'
+  end
+
+  scenario 'deletes album from list' do
+    fill_in 'list_title', with: 'Awesome List'
+    choose 'list_category_personal'
+    click_button 'Create List'
+
+    FactoryGirl.create(:album, title: 'Bad Album')
+    visit albums_path
+    first('.card h3 > a').click
+    select 'Awesome List', from: 'album_list_id'
+    click_button 'Add to List'
+
+    visit lists_path
+    click_link 'Awesome List'
+    click_link 'Delete'
+    expect(page).not_to have_content 'Bad Album'
+  end
+
+  scenario 'moves album position in list' do
+    fill_in 'list_title', with: 'Awesome List'
+    choose 'list_category_personal'
+    click_button 'Create List'
+
+    a1 = FactoryGirl.create(:album, title: 'First Album')
+    a2 = FactoryGirl.create(:album, title: 'Second Album')
+
+    visit album_path(a1)
+    select 'Awesome List', from: 'album_list_id'
+    click_button 'Add to List'
+
+    visit album_path(a2)
+    select 'Awesome List', from: 'album_list_id'
+    click_button 'Add to List'
+
+    visit lists_path
+    click_link 'Awesome List'
+    expect('First').to appear_before 'Second'
+
+    within '.well:last-child' do
+      click_button 'Up'
+    end
+    expect('First').to appear_after 'Second'
+
+    within '.well:first-child' do
+      click_button 'Down'
+    end
+    expect('First').to appear_before 'Second'
+  end
 end
